d9a935fe48de23933244050fc3fd15ae4a1f9717
Farid89-data <frd.ngb.sn@gmail.com>
2025-02-16 15:17:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -31,13 +31,13 @@ def main():
 
         # Create datasets for current session
         train_dataset = IncrementalDataset(
-            root_dir='C:/Users/mehr110/PycharmProjects/Efficient-Memo/dataset/kaggle_data/train',
+            root_dir='C:/Users/mehr110/PycharmProjects/Efficient-Memo/dataset/kaggle_data',
             class_range=range(session * classes_per_session, (session + 1) * classes_per_session),
             transform=transform
         )
 
         val_dataset = IncrementalDataset(
-            root_dir='C:/Users/mehr110/PycharmProjects/Efficient-Memo/dataset/kaggle_data/test',
+            root_dir='C:/Users/mehr110/PycharmProjects/Efficient-Memo/dataset/kaggle_data',
             class_range=range(session * classes_per_session, (session + 1) * classes_per_session),
             transform=transform
         )
